5008c2d2b1966d7d90ad7fb5d9169e32e3b3a30f
Дмитрий Евменов <dimitsky@icloud.com>
2023-01-09 18:28:05 +0400

    Теперь товары можно удалять из корзины

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,8 +1,28 @@
+// redux
+import { useDispatch } from 'react-redux/es/hooks/useDispatch';
+import { removeProduct } from '../../redux/actionsCreators/cartAC';
+
+// react query
+import { useQueryClient } from '@tanstack/react-query';
+
 // my comps
 import { useCartProducts } from '../../hooks/useCartProducts';
 
+// css module
+import classes from './cart.module.css';
+
 function Cart() {
+    const dispatch = useDispatch();
+    const queryClient = useQueryClient();
     const {data: cartProducts, error, status} = useCartProducts();
+
+    // handlers
+    const handleRemove = (id) => {
+        // Удаляем продукт из состояния редакса
+        dispatch(removeProduct(id));
+        // Обновляем кэш tanStack query, чтобы изменения сразу отобразились у клиента
+        queryClient.setQueryData(['cart'], cartProducts => cartProducts.filter(cartProduct => cartProduct._id !== id));
+    };
 
     if (status === 'loading') return (
         <div className="container">
@@ -18,12 +38,31 @@ function Cart() {
 
     return (
         <div className="container">
+
             {
                 cartProducts.map(cartProduct => (
                     <div
+                        className={classes.card}
                         key={cartProduct._id}
                     >
-                        <h2>{cartProduct.name}</h2>
+                        <div className={classes.imgWrap}>
+                            <img 
+                                className={classes.img}
+                                src={cartProduct.pictures} 
+                                alt="Фотография продукта" 
+                            />
+                        </div>
+                        <div className={classes.body}>
+                            <h2 className={classes.name}>{cartProduct.name}</h2>
+                            <span className={classes.price}>{cartProduct.price}₽</span>
+                            <button
+                                className={classes.removeBtn}
+                                type="button"
+                                onClick={() => handleRemove(cartProduct._id)}
+                            >
+                                Удалить
+                            </button>
+                        </div>
                     </div>
                 ))
             }
