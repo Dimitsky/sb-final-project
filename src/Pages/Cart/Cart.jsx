a915b75f65d6370e15bc00cabb5d1a9e065ff90b
Дмитрий Евменов <dimitsky@icloud.com>
2023-01-10 21:16:50 +0400

    Добавил кнопку-пустышку для оформления заказа

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,3 +1,6 @@
+// react
+import { useEffect, useState } from 'react';
+
 // redux
 import { useDispatch, useSelector } from 'react-redux';
 import { chooseProduct, decrementProduct, incrementProduct, removeProduct } from '../../redux/actionsCreators/cartAC';
@@ -12,26 +15,89 @@ import { Counter } from '../../components/Counter/Counter';
 // css module
 import classes from './cart.module.css';
 
+// считает полную цену
+function getFullPrice(cart, data) {
+    return cart.reduce((acc, cartProduct) => {
+        // считает только выбранные товары
+        if (!cartProduct.isChoosed) return acc;
+    
+        // найти данные для товара в корзине
+        const dataProduct = data.find(product => product._id === cartProduct.id);
+        const price = dataProduct.price;
+    
+        // цена * на количество
+        acc += price * cartProduct.count
+        
+        return acc;
+    }, 0)
+}
+
+// считает цену с учетом скидки
+function getDiscountedPrice(cart, data) {
+    return cart.reduce((acc, cartProduct) => {
+        // считает только выбранные товары
+        if (!cartProduct.isChoosed) return acc;
+
+        // найти данные для товара в корзине
+        const dataProduct = data.find(product => product._id === cartProduct.id);
+        // цена
+        const price = dataProduct.price;
+        // скидка (если есть)
+        const discount = dataProduct.discount || null;
+
+        // если скидка есть, то от цены отнять скидку и умножить на количество товаров
+        // либо просто цена * на количество
+        acc += discount ? (price - (price / 100 * discount)) * cartProduct.count : price * cartProduct.count;
+
+        return acc;
+    }, 0)
+}
+
 function Cart() {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
     const queryClient = useQueryClient();
-
     const {data, error, status} = useCartProducts();
+    // полная цена / с учетом скидки
+    const [price, setPrice] = useState({
+        full: 0, 
+        discounted: 0, 
+    });
 
     // handlers
+    // 
     const handleRemove = (id) => {
         // Удаляем продукт из состояния редакса
         dispatch(removeProduct(id));
         // Обновляем кэш tanStack query, чтобы изменения сразу отобразились у клиента
         queryClient.setQueryData(['cart'], data => data.filter(product => product._id !== id));
     };
+    // Отметить товар или снять отметку
     const handleChooseProduct = event => {
         const target = event.target;
         const id = target.id;
 
         dispatch(chooseProduct(id));
     }
+    // кнопка-пустышка оформления заказа
+    const handleOrder = () => {
+        // отправляем заказ только выбранных товаров
+        console.log(cart.filter(cartProduct => cartProduct.isChoosed));
+        console.log(price);
+    }
+
+    useEffect(() => {
+        // если пришли данные с бэка
+        if (!data) return;
+
+        // обновить итоговые цены
+        setPrice({
+            full: getFullPrice(cart, data), 
+            discounted: getDiscountedPrice(cart, data), 
+        })
+        // cart в зависимости нужен, чтобы пересчитывать итоговую цену, 
+        // когда пользователь изменяет количество товаров
+    }, [data, cart]);
 
     if (status === 'loading') return (
         <div className="container">
@@ -54,6 +120,7 @@ function Cart() {
                             className={classes.item}
                             key={product._id}
                         >
+                            {/* тут можем отметить товар */}
                             <input 
                                 className={classes.chooseProduct} 
                                 type="checkbox"
@@ -62,6 +129,7 @@ function Cart() {
                                 checked={cart.find(cartProduct => cartProduct.id === product._id).isChoosed}
                                 onChange={handleChooseProduct} 
                             />
+                            {/* карточка товара */}
                             <div className={classes.card}>
                                 <div className={classes.imgWrap}>
                                     <img 
@@ -73,13 +141,17 @@ function Cart() {
                                 <div className={classes.body}>
                                     <h2 className={classes.name}>{product.name}</h2>
                                     <span className={classes.price}>{product.price}₽</span>
+                                    {/* с помощью Counter клиент может изменять количество конкретного товара */}
                                     <Counter 
+                                        // т.к. клиентский ui зависит от данных в tanStack, а количество, которое пользователь хочет купить,
+                                        // хранится в redux, то приходится для каждого товара из tanStack искать его количество в состоянии redux'а
                                         count={cart.find(cartProduct => cartProduct.id === product._id).count}
                                         maxCount={product.stock}
                                         handlerDecrement={() => dispatch(decrementProduct(product._id))}
                                         handlerIncrement={() => dispatch(incrementProduct(product._id))}
                                     />
                                     <div className={classes.btnWrap}>
+                                        {/* эта кнопка удаляет товар из корзины (так же товар можно удалить из корзины на странице этого товара) */}
                                         <button
                                             className={classes.removeBtn}
                                             type="button"
@@ -95,39 +167,21 @@ function Cart() {
                 }
             </ul>
             {
-                !cart.length ? null : 
-                    <TotalBox 
-                        price={
-                            cart.reduce((acc, cartProduct) => {
-                                if (!cartProduct.isChoosed) return acc;
-
-                                const dataProduct = data.find(product => product._id === cartProduct.id);
-                                const price = dataProduct.price;
-
-                                acc += price * cartProduct.count
-                                
-                                return acc;
-                            }, 0)
-                        }
-                        discountedPrice={
-                            cart.reduce((acc, cartProduct) => {
-                                if (!cartProduct.isChoosed) return acc;
-
-                                const dataProduct = data.find(product => product._id === cartProduct.id);
-                                const price = dataProduct.price;
-                                const discount = dataProduct.discount || null;
-
-                                acc += discount ? (price - (price / 100 * discount)) * cartProduct.count : price * cartProduct.count;
-
-                                return acc;
-                            }, 0)
-                        }
-                    />
+                // если выбранных товаров нет, то скрыть итоговую цену и кнопку "оформить заказ"
+                !cart.filter(cartProduct => cartProduct.isChoosed).length ? null : 
+                    <div className={classes.orderWrap}>
+                        <TotalBox 
+                            price={price.full}
+                            discountedPrice={price.discounted}
+                        />
+                        <OrderBtn handler={handleOrder} />
+                    </div>
             }
         </div>
     );
 }
 
+// выводит итоговую цену без скидки, скидку и и цену с учетом скидки
 function TotalBox({className, price, discountedPrice}) {
     return (
         <div className={ className ? [classes.total, className].join(' ') : classes.total}>
@@ -135,6 +189,18 @@ function TotalBox({className, price, discountedPrice}) {
             <h2 className={classes.discount}>Скидка: -{price - discountedPrice}₽</h2>
             <h2 className={classes.discountedPrice}>Цена со скидкой: {discountedPrice}₽</h2>
         </div>
+    )
+}
+
+// кнопка-пустышка для офрмления товара
+function OrderBtn({handler}) {
+    return (
+        <button
+            className={classes.orderBtn}
+            onClick={handler}
+        >
+            Оформить заказ
+        </button>
     )
 }
 
